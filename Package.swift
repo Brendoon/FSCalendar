0b078b4e6484493ae3b9839414d7ea282990b17e
Brendoon Ryos <brendoon.aboboreira@wirecard.com>
2020-03-21 21:34:35 -0300

    Updating Swift Package Manager file

--- Package.swift
+++ Package.swift
@@ -11,19 +11,12 @@ let package = Package(
             name: "FSCalendar",
             targets: ["FSCalendar"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "FSCalendar",
             path: "FSCalendar",
-            publicHeadersPath: "FSCalendar"),
-        .testTarget(
-            name: "FSCalendarTests",
-            dependencies: ["FSCalendar"]),
+            publicHeadersPath: "FSCalendar")
     ]
 )
